90031647a388a6a73c2eba84920d0c94dfa000d1
Carlos <czelayabaca@gmail.com>
2021-11-24 22:05:42 -0600

    Alcance de función y de bloque

--- Primero.js
+++ Primero.js
@@ -1,16 +1,14 @@
 
-/*
- Scope Global
- Scope local
- */
+function hola(nombre) {
+    if(nombre){
+        var saludo = "Hola " + nombre; //Con var podemos tener el alcance a cualquier parte dentro de la función.
+        //let saludo = "Hola" + nombre;     Aparentemente es el mismo código pero no es así.
+                                        //ya que con let solo tenemos alcance dentro del bloque.
+                                        //Asi que usando let la variable saludo solo estará disonible dentro del bloque IF.
+                                        //Lo mismo pasa con Const.
+    }
 
- var nombre = "Cody";
+    console.log(saludo);
+}
 
- function decirHola(){
-     var nombre = "Carlos";
-     console.log("Hola " + nombre);
- }
-
- decirHola();
-
- console.log(nombre);
+hola("Carlos");
