747fec9ee3e33a21340afe2d2144fa983d1ba0c7
Carlos <czelayabaca@gmail.com>
2021-11-24 21:11:54 -0600

    Seccion 3 - Introduccion a las funciones

--- Primero.js
+++ Primero.js
@@ -1,4 +1,20 @@
-const mensaje = "Aqui estamos empezando a programar en JavaScript, ";
-let nombre = "Carlangas ";
-let apellido = prompt("Cual es tu apellido: ");
-console.log(mensaje + nombre + apellido);
+
+    // Declaración de Función
+function saludar() {
+    console.log("Hola");
+}
+
+function cuadrado(numero) {
+    return numero * numero;
+
+        // Esta línea no  va a ejecutarse, ya que la ejecución de una función termina con un return.
+    // console.log("Hola dev"); 
+}
+
+let numero_de_dos = cuadrado(5);
+
+console.log(numero_de_dos);
+console.log(cuadrado(8));
+
+// Expresión de función
+let func = function(){};
