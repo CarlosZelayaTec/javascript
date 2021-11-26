e23ecb6e5de7d665bcc249ed038e3ac395e5ce36
Carlos <czelayabaca@gmail.com>
2021-11-26 17:25:16 -0600

    Ejercicio de CodeWars - SeniorOrOpen

--- Primero.js
+++ Primero.js
@@ -1,15 +1,65 @@
 
-// let arreglo = [1,2,3,4,5,6];
-// let nombre = "Carlos";
+// Categorizar un nuevo miembro
 
-// console.log(arreglo);
-// console.log(...arreglo); //Spread syntax
-// console.log(...nombre); //Spread syntax
+let edad = [48, 56, 39, 62, 50, 55];
+let handicap = [-2, 24, 22, 5, 13, 10];
 
-// Rest Parameters (...)
+// El Ejercicio con ciclo For
 
-function promedio(alumno, ...calificaciones){
-    console.log(alumno, calificaciones);
-}
+// for (let i = 0; i < edad.length; i++) {
+//     let acum = [];
 
-promedio("Carlos", 9,8,7,9,8,7);
+//     acum[0] = edad[i];
+//     acum[1] = handicap[i];
+
+//     console.log();
+
+//     function senior(){
+//         console.log("[ " + edad[i] + " + " + handicap[i] + " ]" + " = Senior");
+//     };
+    
+//     function open(){
+//         console.log("[ " + edad[i] + " + " + handicap[i] + " ]" + " = Open");
+//     };
+
+//     if(acum[0] >= 55 && acum[1] >= 7 && acum[1] <= 26){
+//         senior();
+//     }else{
+//         open();
+//     }
+// }
+
+// El Ejercicio con funciones y forEach
+
+function edads(){
+    let sitioEdad = [];
+    let sitioHandi = [];
+    edad.forEach(function(edades, indice){
+        if(edades >= 55){sitioEdad[indice] = 1;}
+        else{sitioEdad[indice] = 2;}
+    });
+
+    handicap.forEach(function(handi, indice){
+        if(handi >= 7 && handi <= 26){sitioHandi[indice] = 1}
+        else{sitioHandi[indice] = 2}
+    });
+
+    console.log(sitioEdad);
+    console.log(sitioHandi);
+
+    for (let i = 0; i < edad.length; i++) {
+
+        function senior(){
+            console.log("[ " + edad[i] + " + " + handicap[i] + " ]" + " = Senior");
+        }
+        
+        function open(){
+            console.log("[ " + edad[i] + " + " + handicap[i] + " ]" + " = Open");
+        }
+
+        if(sitioEdad[i] == 1 && sitioHandi[i] == 1){senior();}
+        else open(); }
+    }
+
+
+edads();
