9f741b325dcad10738eabc3dd0ab749948987e71
Carlos <czelayabaca@gmail.com>
2021-11-26 17:49:10 -0600

    Seccion 5 - Declarar un objetos JSON

--- Primero.js
+++ Primero.js
@@ -1,65 +1,17 @@
 
-// Categorizar un nuevo miembro
+// JavaScript Object Notation -> JSON
 
-let edad = [48, 56, 39, 62, 50, 55];
-let handicap = [-2, 24, 22, 5, 13, 10];
+let curso = { //Nombre Objeto
+    titulo: "Curso profesional de JavaScript", //Propiedades
+    formato: "Video",
+    bloques: ["Introducción", "Funciones"],
+    inscribir: function(){ //Tambien llamados metodos
+        console.log("Inscrito");
+    },
+}
 
-// El Ejercicio con ciclo For
+curso.inscribir();
 
-// for (let i = 0; i < edad.length; i++) {
-//     let acum = [];
+curso.titulo = "Curso de React"; //Reescribir el valor de la propiedad.
 
-//     acum[0] = edad[i];
-//     acum[1] = handicap[i];
-
-//     console.log();
-
-//     function senior(){
-//         console.log("[ " + edad[i] + " + " + handicap[i] + " ]" + " = Senior");
-//     };
-    
-//     function open(){
-//         console.log("[ " + edad[i] + " + " + handicap[i] + " ]" + " = Open");
-//     };
-
-//     if(acum[0] >= 55 && acum[1] >= 7 && acum[1] <= 26){
-//         senior();
-//     }else{
-//         open();
-//     }
-// }
-
-// El Ejercicio con funciones y forEach
-
-function edads(){
-    let sitioEdad = [];
-    let sitioHandi = [];
-    edad.forEach(function(edades, indice){
-        if(edades >= 55){sitioEdad[indice] = 1;}
-        else{sitioEdad[indice] = 2;}
-    });
-
-    handicap.forEach(function(handi, indice){
-        if(handi >= 7 && handi <= 26){sitioHandi[indice] = 1}
-        else{sitioHandi[indice] = 2}
-    });
-
-    console.log(sitioEdad);
-    console.log(sitioHandi);
-
-    for (let i = 0; i < edad.length; i++) {
-
-        function senior(){
-            console.log("[ " + edad[i] + " + " + handicap[i] + " ]" + " = Senior");
-        }
-        
-        function open(){
-            console.log("[ " + edad[i] + " + " + handicap[i] + " ]" + " = Open");
-        }
-
-        if(sitioEdad[i] == 1 && sitioHandi[i] == 1){senior();}
-        else open(); }
-    }
-
-
-edads();
+console.log(curso.titulo);
