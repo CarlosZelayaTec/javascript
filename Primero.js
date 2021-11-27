812a330208d34e2d5bde12e355a98fede2e5b7cf
Carlos <czelayabaca@gmail.com>
2021-11-26 21:37:03 -0600

    Seccion 5 - Funciones constructoras

--- Primero.js
+++ Primero.js
@@ -1,44 +1,13 @@
 
-let user = {
-    name: "Carlos",
-    edad: 20,
-    lenguaje: "JavaScript"
+//  Funciones constructoras
+
+function Course(title) {
+    this.title = title;
+    this.inscribir = function(){ }
 }
 
-let { name } = user; //Convierte en una variable la propiedad name.
+let objeto = new Course("JavaScript");
+let js = new Course("Php");
 
-let { edad, ...sobrante } = user; //Saca edad del objeto y las propiedades que queden las enlista en un nuevo objeto
-                                    //llamado sobrante.
-
-function saludar( {name} ) { //Hacemos destructuring para solo capturar la propiedad name del objeto user.
-    console.log(name);
-}
-//// Otra sintaxis.
-function saludarDos( {name: username} ) { //Hacemos destructuring con diferente sintaxis para solo capturar la propiedad name del objeto user.
-    console.log(username);
-}
-
-saludar(user); //Como argumento ponemos todo el objeto, y gracias al destructuring solo capturara el necesario.
-saludarDos(user);
-// console.log(edad);
-// console.log(sobrante);
-
-//Al igual podemos hacer lo mismo con un arreglo 
-
-let calificaciones = [10, 8, 9];
-
-// let [ matematicas, ciencias, biologia ] = calificaciones; // Le asignamos a cada valor una variable.
-// let [ matematicas, ...otrasCarlificaciones] = calificaciones; // Le asignamos a un valor una variable, y a los demas valores los guardamos en otro arreglo.
-
-// console.log(matematicas, ciencias, biologia);
-// console.log(matematicas, otrasCarlificaciones);
-
-//Podemos hacer lo mismo con funciones
-
-function obtenerCalificaciones(){
-    return [10, 8, 9];
-}
-
-let [matematicas, ...otrasCarlificaciones] = obtenerCalificaciones(); //Agarra los valores que retorna la funcion y le hace un destructuring
-
-console.log(matematicas);
+console.log(objeto);
+console.log(js);
