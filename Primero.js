17a76e978f63d44395b37abf5edd5ea6743a2c00
Carlos <czelayabaca@gmail.com>
2021-11-27 08:18:03 -0600

    Seccion 7 - Clases y metodo constructor

--- Primero.js
+++ Primero.js
@@ -1,53 +1,16 @@
 
-let estudiante = {
-    nombre: "Carlos",
-    saludar: function() { console.log("Hola soy " + this.nombre) } /* Con este function podemos reutilizar el this en otros objetos
-                                                                    ya que adoptara el contexto en el que esta.*/
+class Curso{
+
+    #title = "JavaScript";
+
+    constructor(titulo){
+        this.titulo = titulo;
+    }
+
+    inscribir = () => console.log("Inscrito");
 }
 
-let alumno = {
-    nombre: "Sebas",
-    saludo: estudiante.saludar,
-}
+let js = new Curso("Curso de JavaScript");
 
-estudiante.saludar();
-alumno.saludo();
-
-/* ------------------------------------------------------ */
-
-let estudianteDos = {
-    nombre: "Carlos",
-    saludarAlt: () => {console.log("Hola soy " + this.nombre)}, /*Al usar arrow function, se quedará en el contexto de este objeto
-                                                                  así que si intentamos usar este método en otro objeto como lo hicimos
-                                                                  arriba, nos daría un error, ya que el this se queda exclusivamente para
-                                                                  este objeto en específico.  */
-
-                                                                  //Aqui no podemos utilizar el This.
-}
-
-//Utilizando this con arrow function y una clase
-
-function Estudiantes(){
-    this.nombre = "Carlangas";
-    this.saludar = () => {console.log("Hola soy " + this.nombre)};
-}
-
-let e = new Estudiantes();
-
-e.saludar();
-
-//Al usarlo de esta manera podemos utilizarlo como argumento y reasignarlo y su contexto no cambiará, osea no nos dará error.
-
-function Estudiantess(){
-    this.nombre = "Carlangas";
-    this.saludar = function() {console.log(this)};
-
-    // this.saludar = this.saludar.bind(this); // Esta linea me esta confirmando que en todo momento el contexto va a conservarse.
-}
-
-let ee = new Estudiantess();
-
-ee.saludar.call({}, "1", "2"); //metodos para darle valor a this.
-ee.saludar.apply({}, ["1", "2"]);
-
-let nuevaFuncion = e.saludar.bind({}); //Crea y le da un valor a this pero no la ejecuta.
+console.log(js.#title);
+js.inscribir();
